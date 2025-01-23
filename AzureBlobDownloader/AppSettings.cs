6bd0516c3d4063ecdbe40351949936ded8c9dbfd
@senthilecopeland <senthil.elayappan@emerson.com>
2025-01-22 22:40:38 -0500

    Add project files.

---
+++ AzureBlobDownloader/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureBlobDownloader
+{
+    internal class BlobSettings
+    {
+       public string ConnectionString { get; set;}
+       public string ContainerName { get; set; }
+       public string DownloadPath { get; set; }
+    }
+}
---
+++ AzureBlobDownloader/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using AzureBlobDownloader;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+           .WriteTo.Console() 
+           .WriteTo.File("logs/AzureBlobDownloader.txt", rollingInterval: RollingInterval.Day) 
+           .CreateLogger();
+
+Log.Information("Application started.");
+
+var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory()) 
+            .AddJsonFile("appsettings.json") 
+            .Build();
+
+
+var appSettings = configuration.GetSection("BlobSettings").Get<BlobSettings>();
+
+Console.WriteLine("Hello, World!");
+
+Log.Information("Application ended.");
